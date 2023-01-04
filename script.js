73aa206cce6f5ec822ec1c47b8ea73ef14246830
fiona freemark <fiona.freemark@gmail.com>
2023-01-04 13:33:54 -0500

    created namespace/event listener & pulled user inputs

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-// Create a namespace object
-// API Setup 
-// Adding more comments
+//Create a namespace object
+const animalApp = {};
+//Grab our form with JS
+animalApp.formSubmit = document.querySelector('form');
+//Add event listener which listens for form submit
+animalApp.formSubmit.addEventListener('submit', function (event) { 
+
+    // Prevent default behavior (Page refresh)
+    event.preventDefault();
+    
+    // Get the value of the inputs the user has selected:
+
+    // Targeting Activity Input
+    animalApp.activityInput = document.querySelector('input[name=favorite-activity]:checked');
+    animalApp.logActivity = animalApp.activityInput.value;
+    console.log(animalApp.logActivity);
+
+    // Targeting Animal Input
+    animalApp.animalInput = document.querySelector('input[name="animal-type"]:checked');
+    animalApp.logAnimal = animalApp.animalInput.value;
+    console.log(animalApp.logAnimal);
+
+});
